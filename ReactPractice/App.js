90ab4efcc160bf7f1b8d0399ce50dbd213ee238b
yujinroh <yujinroh37@gmail.com>
2023-10-18 14:22:32 +0900

    update: combine React, VanillaJS, Tailwind folders

---
+++ ReactPractice/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ChangePropsValue from './ChangePropsValue';
+function App() {
+return (
+<>
+<ChangePropsValue name="SOOA"/>
+</>
+);
+};
+export default App;
---
+++ ReactPractice/ChangePropsValue.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function ChangePropsValue(props) {
+let name = props.name;
+function changeName(){
+name = "React";
+}
+return (
+<div>
+<h1>{props.name}</h1>
+<button onClick={changeName}>클릭</button>
+</div>
+);
+};
+export default ChangePropsValue;
