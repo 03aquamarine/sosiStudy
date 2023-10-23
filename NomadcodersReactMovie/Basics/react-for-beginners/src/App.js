8b8ba7ccbdd90bbbd14ba69f0378249cc826e9e2
yujinroh <yujinroh37@gmail.com>
2023-10-23 18:03:07 +0900

    feat: #7.6 Parameters (ing)

--- NomadcodersReactMovie/Basics/react-for-beginners/src/App.js
+++ NomadcodersReactMovie/Basics/react-for-beginners/src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/hello" element={<h1>Hello</h1>} />
+        <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
